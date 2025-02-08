cd1637d05bbcb1054665e2334cfc31a332401d9c
Brblal <kleandr.p@seznam.cz>
2025-02-08 15:21:11 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -88,4 +88,5 @@ def login():
 if __name__ == '__main__':
     db.init_db()  # Inicializace databáze při spuštění aplikace
     connect_and_subscribe()
-    app.run(debug=True)
+    
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
